c146fa1a6dc4a9f4730de75a384f94fa8ef6f371
Sergey Korobko <sergey.korobko@bostongene.com>
2025-09-06 12:28:58 +0400

    initial form

---
+++ script.js
@@ -0,0 +1,29 @@
+document
+  .getElementById("myForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const apiToken = "1aee7a110b2fc4a5fef4f9304b2eec83c0c12a4f";
+
+    try {
+      const response = await fetch(
+        `https://api.pipedrive.com/api/v2/deals?api_token=${apiToken}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title }),
+        }
+      );
+
+      if (response.ok) {
+        alert("Успешно отправлено!");
+      } else {
+        alert("Ошибка: " + response.status);
+      }
+    } catch (error) {
+      alert("Произошла ошибка: " + error.message);
+    }
+  });
